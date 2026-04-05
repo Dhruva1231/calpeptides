a3c81b54c6fce13e789d3fc6950b58e2413fd584
Dhruva <dhruvasharma@berkeley.edu>
2026-04-05 01:16:56 -0700

    fixed

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,21 @@
+// main.tsx or index.tsx
 import { createRoot } from "react-dom/client";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import App from "./App.tsx";
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import FAQ from "./pages/FAQ";
+import About from "./pages/About";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/shop" element={<Shop />} />
+      <Route path="/faq" element={<FAQ />} />
+      <Route path="/about" element={<About />} />
+      {/* Add other routes if needed */}
+    </Routes>
+  </HashRouter>
+);
